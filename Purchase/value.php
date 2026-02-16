64d3e79f516e1ec7a9e9c5d1464397a799d0a415
Sowmya41724 <sowmyakumarn2004@gmail.com>
2026-02-16 17:32:41 +0530

    little change in value

--- Purchase/value.php
+++ Purchase/value.php
@@ -4,7 +4,7 @@ $q = isset($_GET['q']) ? intval($_GET['q']) : 0;
 include "../config.php";
 
 $sql = "SELECT unitValue, Rate FROM Product WHERE id = ?";
-$stmt = mysqli_prepare($con, $sql);
+$stmt = mysqli_prepare($conn, $sql);
 mysqli_stmt_bind_param($stmt, "i", $q);
 mysqli_stmt_execute($stmt);
 $result = mysqli_stmt_get_result($stmt);
@@ -15,5 +15,5 @@ if ($row = mysqli_fetch_assoc($result)) {
     ;
 }
 
-mysqli_close($con);
+mysqli_close($conn);
 ?>
